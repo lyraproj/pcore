1e901a750f4385beb9592a9f9bb3321f36e4cab9
Thomas Hallgren <thomas@tada.se>
2019-04-01 07:55:00 +0100

    Move function 'new' from puppet-evaluator to pcore.
    The 'new' function may be used by a Deferred when instantiating default
    values of a type. It must therefore be considered part of the pcore type
    system rather than part of the evaluator.

--- pximpl/function.go
+++ pximpl/function.go
@@ -388,4 +388,15 @@ func init() {
 	px.MakeGoConstructor2 = func(typeName string, localTypes px.LocalTypesCreator, creators ...px.DispatchCreator) px.ResolvableFunction {
 		return buildFunction(typeName, localTypes, creators)
 	}
+
+	px.NewGoFunction(`new`,
+		func(d px.Dispatch) {
+			d.Param(`Variant[Type,String]`)
+			d.RepeatedParam(`Any`)
+			d.OptionalBlock(`Callable[1,1]`)
+			d.Function2(func(c px.Context, args []px.Value, block px.Lambda) px.Value {
+				return px.NewWithBlock(c, args[0], args[1:], block)
+			})
+		},
+	)
 }
