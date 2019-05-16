13c31a5f9c99c66fbf6a4d3d38d7ce7e445fb00d
Thomas Hallgren <thomas@tada.se>
2019-05-16 18:20:56 +0200

    Add yaml.UnmarshalWithLocations() function
    This new function will parse yaml into object values that have a line
    and column attribute so that errors in Lyra Yaml workflows or Hiera
    can be reported with proper line and column information.

--- yaml/unmarshal_test.go
+++ yaml/unmarshal_test.go
@@ -10,8 +10,7 @@ import (
 	"github.com/lyraproj/pcore/yaml"
 )
 
-const text = `
-foo:
+const text = `foo:
   bar: [1, 2, {hello: sub}]
 bar:
   - 1
@@ -22,5 +21,18 @@ func TestUnmarshal(t *testing.T) {
 	pcore.Do(func(c px.Context) {
 		v := yaml.Unmarshal(c, []byte(text))
 		require.Equal(t, `{'foo' => {'bar' => [1, 2, {'hello' => 'sub'}]}, 'bar' => [1, 2]}`, v.String())
+	})
+}
+
+func TestUnmarshalWithPositions(t *testing.T) {
+	pcore.Do(func(c px.Context) {
+		v := yaml.UnmarshalWithPositions(c, []byte(text))
+		require.Equal(t, `{'foo' => {'bar' => [1, 2, {'hello' => 'sub'}]}, 'bar' => [1, 2]}`, v.String())
+		bar, ok := v.Value.(px.OrderedMap).Get4(`bar`)
+		require.True(t, ok)
+		barPos, ok := bar.(*yaml.Value)
+		require.True(t, ok)
+		require.Equal(t, 4, barPos.Line)
+		require.Equal(t, 3, barPos.Column)
 	})
 }
